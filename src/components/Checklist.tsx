315b1a564d46ecb6e9c08bb622d844750999be8d
Jing <jing@caravan.coop>
2024-02-11 15:10:14 -0500

    add uuid to dynamically generate id in tsx

--- src/components/Checklist.tsx
+++ src/components/Checklist.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
+import { v5 as uuidv5 } from 'uuid';
 
 interface ChecklistItem {
-  id: number;
   label: string;
   details?: string;
-  completed: boolean;
 }
 
 interface ChecklistItems {
@@ -13,35 +12,63 @@ interface ChecklistItems {
 }
 
 const Checklist: React.FC<{ checklist: ChecklistItems[] }> = ({ checklist }) => {
-  const checklistTotalItems = checklist.reduce((acc, category) => acc + category.contents.length, 0);
-  const [checkedCount, setCheckedCount] = useState<number>(0);
-  const [isChecked, setIsChecked] = useState<boolean[]>([]);
+  const NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+  const generateUUID = (label: string) => uuidv5(label, NAMESPACE);
 
-  const handleCheckboxChange = (index: number) => {
-    const newIsChecked = [...isChecked];
-    newIsChecked[index] = !isChecked[index];
-    setCheckedCount(newIsChecked.filter(Boolean).length);
-    setIsChecked(newIsChecked);
+  const listWithIds = checklist.map((category) => ({
+    ...category,
+    contents: category.contents.map((content) => ({
+      ...content,
+      uid: generateUUID(content.label)  as string
+    }))
+  }));
+
+   const [checkedItems, setCheckedItems] = useState<{[key: string]: boolean}>(
+    listWithIds.reduce((acc, category) => {
+      category.contents.forEach((item) => {
+        acc[item.uid] = false;
+      });
+      return acc;
+    }, {} as {[key: string]: boolean})
+  );
+
+  const [checkedCount, setCheckedCount] = useState<number>(0);
+  const totalCount = Object.keys(checkedItems).length;
+
+  const handleCheckboxChange = (uid: string) => {
+    setCheckedItems(prevCheckedItems => {
+      const updatedCheckedItems = {
+        ...prevCheckedItems,
+        [uid]: !prevCheckedItems[uid],
+      };
+      setCheckedCount(Object.values(updatedCheckedItems).filter(Boolean).length);
+      return updatedCheckedItems;
+    });
+    setCheckedCount(Object.values(checkedItems).filter(Boolean).length);
   };
 
   const handleUncheckAll = () => {
-    setIsChecked(new Array(checklist.length).fill(false));
+    const uncheckedItems: {[key: string]: boolean} = {};
+    Object.keys(checkedItems).forEach((key) => {
+      uncheckedItems[key] = false;
+    });
+    setCheckedItems(uncheckedItems);
     setCheckedCount(0);
   };
 
   return (
     <div>
-      {checklist.map((categoryItem, categoryIndex) => (
+      {listWithIds.map((categoryItem, categoryIndex) => (
         <div key={categoryIndex} className='mb-4'>
           <h2 className='text-lg capitalize text-chk-smoke'>{categoryItem.category}</h2>
           <ul>
             {categoryItem.contents.map((item, itemIndex) => (
               <li key={itemIndex}>
                 <div className='inline-flex items-center'>
-                  <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor={`checkbox-${item.id}`}>
-                    <input type="checkbox" id={`checkbox-${item.id}`}
-                      checked={isChecked[categoryIndex * categoryItem.contents.length + itemIndex]}
-                      onChange={() => handleCheckboxChange(categoryIndex * categoryItem.contents.length + itemIndex)}
+                  <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor={`checkbox-${item.uid}`}>
+                    <input type="checkbox" id={`checkbox-${item.uid}`}
+                      checked={checkedItems[item.uid] || false}
+                      onChange={() => handleCheckboxChange(item.uid)}
                       className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none
                       rounded-md border-2 border-chk-teal transition-all
                       before:absolute before:top-2/4 before:left-2/4 before:block before:h-12
@@ -52,15 +79,15 @@ const Checklist: React.FC<{ checklist: ChecklistItems[] }> = ({ checklist }) => 
                     <div className="absolute text-white transition-opacity opacity-0 pointer-events-none
                       top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="white"
-                        stroke="white" stroke-width="1">
-                        <path fill-rule="evenodd"
+                        stroke="white" strokeWidth="1">
+                        <path fillRule="evenodd"
                           d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clip-rule="evenodd"></path>
+                          clipRule="evenodd"></path>
                       </svg>
                     </div>
                   </label>
                   <div>
-                    <label htmlFor={`checkbox-${item.id}`} className='block text-chk-charcoal capitalize cursor-pointer'>{item.label}</label>
+                    <label htmlFor={`checkbox-${item.uid}`} className='block text-chk-charcoal capitalize cursor-pointer'>{item.label}</label>
                     {item.details && <span className="block text-xs text-chk-smoke">{item.details}</span>}
                   </div>
                 </div>
@@ -69,7 +96,7 @@ const Checklist: React.FC<{ checklist: ChecklistItems[] }> = ({ checklist }) => 
           </ul>
         </div>
       ))}
-      <p className='text-chk-charcoal'><span className='text-chk-teal'>{checkedCount}</span> out of {checklistTotalItems} checked</p>
+      <p className='text-chk-charcoal'><span className='text-chk-teal'>{checkedCount}</span> out of {totalCount} checked</p>
       <button onClick={handleUncheckAll} className='rounded-md py-2 px-6 bg-chk-teal text-chk-cream'>Uncheck All</button>
     </div>
   );
