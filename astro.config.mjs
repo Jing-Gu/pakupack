18a100ef51efa7488ad9e36f26c580c656141ba1
Jing <jing@caravan.coop>
2024-02-14 15:25:51 -0500

    fix: built no styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
+ //https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/Jing-Gu/pakupack',
-  //to change to real, no base needed
+  base: '/public',
+  output: 'static',
+  //to change to real
   //site: 'https://www.pakupack.app'
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
